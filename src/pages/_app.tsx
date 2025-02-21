822f565e6de7faafd8fb2c6e53e3fb60b1e23de3
Zayd Syed <zaydsyed9@gmail.com>
2025-02-21 04:58:06 -0500

    changed title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <title>Zayd Syed</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
   );
 }
